55d6716ad5098834dbf86415b0f4de8262f4b92c
Zilin Ye <yezilin2014@outlook.com>
2022-03-12 22:54:18 -0500

    Data clean done

---
+++ Cleaner/data_clean.py
@@ -0,0 +1,46 @@
+from distutils.log import warn
+import imp
+import string
+import pandas as pd
+import glob
+from warn import bcolors
+# import os.path
+class Clean:
+    def __init__(self,pk,path):
+        self.pk = pk
+        print("Loading File From Folder and import into Pandas")
+        for file in glob.glob(path):
+            file_path = file
+        self.initfile = pd.read_excel(file_path)
+        self.initfile = self.initfile.drop(columns=['BIRTHDATE','SEX','ACADEMIC_LOAD','COUNTRY','Phone/WhatApps','Address','HOME_EMAIL','CHARGES','PYMTS','Outstanding Fee','Comment'])
+
+
+    def CheckDuplicate(self):
+        print("check Duplicate Data from files")
+        if len(self.initfile[self.initfile.duplicated(subset=self.pk)])>0:
+            print(f"{bcolors.WARNING}Fund {str(len(self.initfile[self.initfile.duplicated(subset=self.pk)]))} duplicated record(s) !!!{bcolors.ENDC}")
+            return True,self.initfile[self.initfile.duplicated(subset=self.pk,keep=False)]
+        else:
+            return False,self.initfile
+
+
+    def CheckNameWithoutComma(self):
+        print("check Student Name without comma")
+        nocomma_name = True
+        while nocomma_name:
+            nocomma = self.initfile[~self.initfile['NAME'].str.contains(',', na=False)]
+            nocomma_name = len(nocomma)>0
+            if nocomma_name:
+                print(f"{bcolors.WARNING}Fund {str(len(nocomma))} no-comma included record(s) !!!{bcolors.ENDC}")
+                print(nocomma)
+                action = int(input("choose action : \n1.Auto Fix (add comma at first space location)\n2.Manully fix\n:"))
+                if action == 1:
+                    indexlist = nocomma.index
+                    for index in indexlist:
+                        self.initfile.at[index,'NAME'] = self.initfile.at[index,'NAME'].replace(' ',',',1)
+                    
+                else:
+                    quit()
+                
+        return nocomma_name,self.initfile
+                
---
+++ main.py
@@ -0,0 +1,14 @@
+import warnings
+from Cleaner.data_clean import Clean
+def main():
+    print("Start from Begin")
+    initData = Clean(pk='EMPLID',path='Files/Fall/Enrolled*.xlsx')
+    duplicated,record = initData.CheckDuplicate()
+    if duplicated:
+        print(record)
+        print('Quitting the program')
+        quit()
+    nocomma,record = initData.CheckNameWithoutComma()
+    print(record)
+
+main()
