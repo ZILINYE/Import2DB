6d1e7b66ae1b15d99aa2b777818910db11bb0b8f
Zilin Ye <yezilin2014@outlook.com>
2022-03-19 00:19:41 -0400

    upload DB

--- DataProcess/tomaria.py
+++ DataProcess/tomaria.py
@@ -11,7 +11,7 @@ class Maria:
         self.dataf = dataf
         try:
             sqlEngine = create_engine(
-                "mysql+pymysql://it:Acumen321@192.168.5.235/Ace", pool_recycle=3600
+                "mysql+pymysql://root:YZLmm1994@192.168.3.73/Test", pool_recycle=3600
             )
 
         except:
@@ -22,17 +22,18 @@ class Maria:
         # Get Cursor
         self.cursor = sqlEngine.connect()
 
-    def GetProgramInfo(self, Pcode):
+    def GetProgramInfo(self, Pcode) -> pd.DataFrame:
         condition = ""
         if len(Pcode) > 0:
             condition = "WHERE Program_code='%s'" % Pcode
         sql = "SELECT Program_code,Program_desc FROM ProgramInfo " + condition
-        df = pd.read_sql(sql, self.cursor)
-        print(df)
-        result = df.to_json(orient="records")
-        parsed = json.loads(result)
-        jsonlist = json.dumps(parsed, indent=4)
-        print(jsonlist)
+        programinfo = pd.read_sql(sql, self.cursor)
+        return programinfo
+        # print(df)
+        # result = df.to_json(orient="records")
+        # parsed = json.loads(result)
+        # jsonlist = json.dumps(parsed, indent=4)
+        # print(jsonlist)
     def GetStudentInfo(self,Sid) -> pd.DataFrame: 
         condition = ""
         if len(Sid) > 0:
@@ -40,53 +41,22 @@ class Maria:
         sql = "SELECT * FROM StudentInfo " + condition
         studentinfo = pd.read_sql(sql, self.cursor)
         return studentinfo
-    
-    def ImportMariadb(self):
-        pd.set_option('display.max_columns', 500)
-        pd.set_option('display.width', None)
-        pd.set_option('display.max_colwidth', None)
+
+    def ImportStudentInfo(self):
+        # pd.set_option('display.max_columns', 500)
+        # pd.set_option('display.width', None)
+        # pd.set_option('display.max_colwidth', None)
+
+       
+        # import student info function
         studentondb = self.GetStudentInfo(Sid='')
-        self.dataf[["Term", "TermYear"]] = self.dataf.TERMDESC.str.split(
-            " ",
-            expand=True,
-        )
-        self.dataf[
-            ["Program_code", "Semester"]
-        ] = self.dataf.ACAD_PROG_PRIMARY.str.split(
-            " ",
-            expand=True,
-        )
-        self.dataf[["Firstname", "Lastname"]] = self.dataf.NAME.str.split(
-            ",",
-            expand=True,
-        )
-        self.dataf["Semester"] = self.dataf["Semester"].str[-1:]
-        self.dataf['EMPLID'] = self.dataf['EMPLID'].apply(str)
-        self.dataf["StudentID"] = "W0" + self.dataf["EMPLID"]
-        self.dataf['BIRTHDATE'] = self.dataf["BIRTHDATE"].astype(str).str[0:10]
-        self.dataf =self.dataf.drop(
-            columns=[
-                "TERMDESC",
-                "ACAD_PROG_PRIMARY",
-                "EMPLID",
-                "PROG_DESCR",
-            ]
-        )
-        self.dataf = self.dataf.rename(
-            columns={
-                "CAMPUS": "CampusCode",
-                "Class": "Section",
-                "NAME": "Fullname",
-                "BIRTHDATE": "Birthday",
-                "SEX": "Gender",
-                "COUNTRY": "Country",
-                "Phone/WhatApps": "ContactInfo",
-                "CAMP_EMAIL": "CampEmail",
-                "HOME_EMAIL":"HomeEmail",
-            }
-        )
-        studentInfo = self.dataf.drop(columns=['Section','CampusCode','Term','TermYear','Semester','Program_code']).rename(columns={'StudentID':'ID'})
+        studentInfo = self.dataf.drop(columns=['Section','Campus_code','Term','TermYear','Semester','Program_code']).rename(columns={'StudentID':'ID'})
         droplist= studentondb['ID'].tolist()
         differ = pd.concat([studentInfo,studentondb]).drop_duplicates(subset=['ID']).set_index('ID').drop(droplist)
         differ.to_sql(name='StudentInfo',con=self.cursor,if_exists='append')
         # print(studentInfo)
+    def ImportStudentProgramInfo(self):
+
+
+        studentproinfo = self.dataf.drop(columns=['Fullname','Firstname','Lastname','CampEmail','HomeEmail','Gender','Birthday','Address','Country','ContactInfo'],errors='ignore').set_index('StudentID')
+        studentproinfo.to_sql(name='StudentProgram',con=self.cursor,if_exists='append')
--- main.py
+++ main.py
@@ -4,16 +4,18 @@ from DataProcess.tomaria import Maria
 
 def main():
     print("Start from Begin")
-    initData = Clean(path='Files/Spring/Enrolled*.xlsx')
+    initData = Clean(path='Files/Fall/Enrolled*.xlsx')
     duplicated, record = initData.CheckDuplicate(subset='EMPLID')
     if duplicated:
         print(record)
         print('Quitting the program')
         quit()
-    nocomma, record = initData.CheckNameWithoutComma(column='NAME')
-    db = Maria(record)
+    record = initData.CheckNameWithoutComma(column='NAME')
+    formatted = initData.FormatDf()
+    db = Maria(formatted)
     # db.GetProgramInfo(Pcode='M018')
-    db.ImportMariadb()
+    db.ImportStudentInfo()
+    db.ImportStudentProgramInfo()
 
 
 
